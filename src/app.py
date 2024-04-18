9f9c3fefcdda4755d1f273234b89517cf29a0139
Arthur Berezin <rtpro1@gmail.com>
2024-04-18 23:17:24 +0300

    Fix import statement in app.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 import logging
 from slack_bolt import App
 from slack_sdk.web import WebClient
-from .onboarding_tutorial import OnboardingTutorial
+from onboarding_tutorial import OnboardingTutorial
+
 
 # Initialize a Bolt for Python app
 app = App()
